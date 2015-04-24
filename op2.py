d03221296ba487ce06093f4f7002662109ada16c
jarikoosting <oostingjarik@gmail.com>
2015-04-24 09:17:13 +0200

    Made it better

--- op2.py
+++ op2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import nltk
 from nltk.corpus import brown
+from nltk import FreqDist
 from nltk.tag import UnigramTagger
 
+words = brown.tagged_words(categories='mystery')
+
 # Exercise 2A
-print(len(brown.tagged_words(categories='mystery'))) #57169
+print(len(words)) #57169
 print(len(brown.tagged_sents(categories='mystery'))) #3886
 
 # Exercise 2B
@@ -12,9 +15,8 @@ print(brown.tagged_words(categories='mystery')[99]) # I, PPSS
 print(brown.tagged_words(categories='mystery')[100]) # Suddenly, RB
 
 # Exercise 2C
-words = brown.tagged_words(categories='mystery')[:500]
-listOfWords = []
-[listOfWords.append(item[1]) for item in words if item[1] not in listOfWords]
-print(len(listOfWords)) # 61
+listOfTags = []
+[listOfTags.append(item[1]) for item in words if item[1] not in listOfTags]
+print(len(listOfTags)) # 169 tags
 
 # Exercise 2D
